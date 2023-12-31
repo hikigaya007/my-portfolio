7885b5967209b9427d0b6a3c49b0fd9dfba5f6f3
hikigaya007 <bijop007@gmail.com>
2023-12-31 21:45:37 +0530

    initiated project : Project setup

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    
+    <div className='w-full bg-slate-900 text-white flex  flex-col justify-center items-center'>
+      <section>Main page</section>
+      <section>Parallax</section>
+      <section>skills</section>
+      <section>projects parallax</section>
+      <section>prj 1</section>
+      <section>prj 2</section>
+      <section>prj 3</section>
+      <section>Contact me</section>
+    </div>    
+  )
+}
+
+export default App
