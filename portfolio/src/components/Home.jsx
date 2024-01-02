c65f72a5f1e35df6c0276e463ce53658eabc2422
hikigaya007 <bijop007@gmail.com>
2024-01-02 22:58:05 +0530

    Hero section almost completed

--- portfolio/src/components/Home.jsx
+++ portfolio/src/components/Home.jsx
@@ -4,6 +4,7 @@ import { faBars } from '@fortawesome/free-solid-svg-icons';
 import {motion} from 'framer-motion';
 import profile from '../assets/profile.jpeg'
 import '../App.css'
+import { TypeAnimation } from 'react-type-animation';
 
 function Home() {
 
@@ -42,10 +43,10 @@ function Home() {
             <li><a href="/">Projects</a></li>
             
         </ul>
-        <div className='md:hidden cursor-pointer z-10 bg-white p-2 rounded-full w-[36px] flex items-center justify-center'
+        <div className='md:hidden cursor-pointer z-20 bg-white p-2 rounded-full w-[36px] flex items-center justify-center'
         onClick={handleClick}><FontAwesomeIcon icon={faBars} size='lg' style={{color:'black'}} /></div>
         
-        {collapseNav && <ul className='bg-white  text-black absolute top-0 left-0 flex flex-col h-screen w-full justify-center items-center'>
+        {collapseNav && <ul className='bg-white z-10 text-black absolute top-0 left-0 flex flex-col h-screen w-full justify-center items-center'>
                 {links.map((value , index) =>{
                     return(
                         <li className='py-6 text-3xl hover:scale-105 cursor-pointer transition-all duration-200' key={index}>{value.title}</li>
@@ -57,14 +58,22 @@ function Home() {
 
     </div>
 
-        <div className='flex flex-col items-center max-w-6xl md:items-center md:flex-row md:justify-center lg:gap-32 mx-auto mt-5 lg:h-[80%] md:h-[80%]'>
+        <div className='flex flex-col items-center justify-center max-w-7xl md:mt-2 mx-auto  h-[80%] '>
             <div className='custom-animation'><img src={profile} alt="Profile Picture" className=' size-40 md:size-80 sm:size-60 rounded-full shadow-purple-400 shadow-lg ' /></div>
-            <div>
-                    <h2 className="font-semibold text-4xl sm:text-5xl md:text-6xl">
+            <div className='mt-4'>
+                    <h2 className="font-semibold text-4xl sm:text-5xl md:text-6xl text-center">
                             Hi! I'm <span className=" text-violet-400">Bijo </span>Prasad
                             <br />Full Stack
-                            <span className=" text-violet-400"> Developer</span>
-                    </h2>
+                            <span className=" text-violet-400"> <TypeAnimation
+                                sequence={['Developer', 1000, 'Designer', 1000]}
+                                speed={20}
+                                repeat={Infinity}
+                                /></span>
+                    </h2><br/>
+                    <p className="text-base sm:text-lg md:text-xl text-center">
+                        My <span className="text-violet-400"> favorite tech</span> includes JavaScript , NextJs
+                        ,TailwindCSS , Node.js + Express.js & MongoDB!
+                    </p>
                     
             </div>
         </div>
